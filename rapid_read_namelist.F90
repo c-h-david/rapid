0b5529ce174bc8df65c31a49d5cea6325a670747
Cedric H. David <chdavid@uci.edu>
2014-03-31 15:33:51 -0700

    Added subroutine to read rapid_namelist

---
+++ rapid_read_namelist.F90
@@ -0,0 +1,66 @@
+subroutine rapid_read_namelist
+
+!PURPOSE
+!This subroutine allows to read the RAPID namelist and hence to run the model
+!multiple times without ever have to recompile.  Some information on the options
+!used is also printed in the stdout
+!Author: Cedric H. David, 2011 
+
+
+!*******************************************************************************
+!Declaration of variables
+!*******************************************************************************
+use rapid_var, only :                                                          &
+                     BS_opt_Qinit,BS_opt_forcing,                              &
+                     IS_opt_routing,IS_opt_run,IS_opt_phi,                     &
+                     IS_reachtot,modcou_connect_file,m3_nc_file,IS_max_up,     &
+                     IS_reachbas,basin_id_file,                                &
+                     Qinit_file,                                               &
+                     IS_forcingtot,forcingtot_id_file,Qfor_file,               &
+                     IS_forcinguse,forcinguse_id_file,                         &
+                     k_file,x_file,Qout_nc_file,                               &
+                     kfac_file,xfac_file,ZS_knorm_init,ZS_xnorm_init,          &
+                     IS_gagetot,Qobs_file,gage_id_file,Qobsbarrec_file,        &
+                     ZS_TauM,ZS_dtM,ZS_TauO,ZS_dtO,ZS_TauR,ZS_dtR,             &
+                     ZS_phifac,IS_strt_opt,                                    &
+                     NL_namelist,namelist_file,                                &
+                     rank
+
+
+implicit none
+
+
+!*******************************************************************************
+!Read namelist file 
+!*******************************************************************************
+open(88,file=namelist_file,status='old',form='formatted')
+read(88, NL_namelist)
+close(88)
+
+
+!*******************************************************************************
+!Optional prints what was read 
+!*******************************************************************************
+!print *, namelist_file
+!print *, 'IS_reachtot=', IS_reachtot
+!print *, 'IS_reachbas=', IS_reachbas
+!print *, 'basin_id_file', basin_id_file
+!print *, 'Qinit_file', Qinit_file
+!print *, 'Qfor_file', Qfor_file
+!print *, ZS_TauM,ZS_dtM,ZS_TauO,ZS_dtO,ZS_TauR,ZS_dtR
+!print *, IS_strt_opt
+!print *, IS_max_up
+
+if (rank==0 .and. .not. BS_opt_Qinit)   print *, 'Not initializing flows'
+if (rank==0 .and. BS_opt_Qinit)         print *, 'Initializing flows'
+if (rank==0 .and. .not. BS_opt_forcing) print *, 'Not using forcing'
+if (rank==0 .and. BS_opt_forcing)       print *, 'Using forcing'
+if (rank==0 .and. IS_opt_routing==1)    print *, 'Routing with matrix-based Muskingum method'
+if (rank==0 .and. IS_opt_routing==2)    print *, 'Routing with traditional Muskingum method'
+if (rank==0 .and. IS_opt_run==1)        print *, 'RAPID mode: computing flowrates'
+if (rank==0 .and. IS_opt_run==2)        print *, 'RAPID mode: optimizing parameters'
+!if (rank==0 .and. IS_opt_phi==1)        print *, 'RAPID optimizing using phi1' 
+!if (rank==0 .and. IS_opt_phi==2)        print *, 'RAPID optimizing using phi2' 
+
+
+end subroutine rapid_read_namelist
