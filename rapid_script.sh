08292dfcc7cc6aeb57a4e97105b8e2bf6c2f3eca
Cedric H. David <chdavid@uci.edu>
2014-04-01 14:27:08 -0700

    Removed all unnecessary options

--- rapid_script.sh
+++ rapid_script.sh
@@ -1,19 +1,9 @@
 FILE=$(date +"%Y-%m-%d_%H-%M-%S_rapid_stdout.txt")
-/usr/bin/time mpiexec -n 1 ./rapid                                             \
-                           -ksp_type richardson                                \
-                           1>$FILE 2>>$FILE
-#-ksp_type preonly
-#-ksp_type richardson 
-#-ksp_monitor
-#-ksp_view
-#-log_summary
-#-info
-#-pc_type sor  
-#-pc_type sor -pc_sor_backward 
-#-pc_type sor -pc_sor_forward 
-#-pc_type bjacobi -sub_pc_type sor -sub_pc_sor_forward 
-#-mat_type mpiaij -ksp_type preonly -pc_type lu -pc_factor_mat_solver_package mumps
-#-pc_factor_reuse_ordering -pc_factor_reuse_fill
+/usr/bin/time mpiexec                  \
+              -n 1                     \
+              ./rapid                  \
+              -ksp_type richardson     \
+              1>$FILE 2>>$FILE
 
 #FILE is a name created based on the time when the model started running
 #FILE contains stdout from running the model (through 1), but also stderr 
