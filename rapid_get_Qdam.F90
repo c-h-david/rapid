f7f0fd59226a4a3a8f112a484d920b940ab519f5
Cedric H. David <chdavid@uci.edu>
2014-04-03 12:40:34 -0700

    New subroutine

---
+++ rapid_get_Qdam.F90
@@ -0,0 +1,107 @@
+!*******************************************************************************
+!Subroutine - rapid_get_Qdam
+!*******************************************************************************
+subroutine rapid_get_Qdam
+
+!Purpose:
+!Communicate with a dam subroutine to exchange inflows and outflows.
+!Author: 
+!Cedric H. David, 2013.
+
+
+!*******************************************************************************
+!Global variables
+!*******************************************************************************
+use rapid_var, only :                                                          &
+                   rank,ierr,vecscat,ZV_pointer,ZV_SeqZero,ZS_one,             &
+                   IS_dam_bas,                                                 &
+                   ZM_Net,ZV_Qext,ZV_Qdam,                                     &
+                   ZV_Qin_dam,ZV_Qout_dam,                                     &
+                   ZV_QoutbarR,ZV_QinbarR,                                     &
+                   IV_dam_loc2,IV_dam_index,IV_dam_pos
+
+
+implicit none
+
+
+!*******************************************************************************
+!Includes
+!*******************************************************************************
+#include "finclude/petscsys.h"       
+!base PETSc routines
+#include "finclude/petscvec.h"  
+#include "finclude/petscvec.h90"
+!vectors, and vectors in Fortran90 
+#include "finclude/petscmat.h"    
+!matrices
+#include "finclude/petscksp.h"    
+!Krylov subspace methods
+#include "finclude/petscpc.h"     
+!preconditioners
+#include "finclude/petscviewer.h"
+!viewers (allows writing results in file for example)
+#include "finclude/petsclog.h" 
+!PETSc log
+
+
+!*******************************************************************************
+!Intent (in/out), and local variables 
+!*******************************************************************************
+
+
+!*******************************************************************************
+!Read file
+!*******************************************************************************
+if (rank==0) ZV_Qin_dam=0 
+call MatMult(ZM_Net,ZV_QoutbarR,ZV_QinbarR,ierr)           
+call VecAXPY(ZV_QinbarR,ZS_one,ZV_Qext,ierr)
+!QinbarR=Net*QoutbarR+Qext
+call VecScatterBegin(vecscat,ZV_QinbarR,ZV_SeqZero,                            &
+                     INSERT_VALUES,SCATTER_FORWARD,ierr)
+call VecScatterEnd(vecscat,ZV_QinbarR,ZV_SeqZero,                              &
+                        INSERT_VALUES,SCATTER_FORWARD,ierr)
+call VecGetArrayF90(ZV_SeqZero,ZV_pointer,ierr)
+if (rank==0) ZV_Qin_dam=ZV_pointer(IV_dam_pos) 
+call VecRestoreArrayF90(ZV_SeqZero,ZV_pointer,ierr)
+!Get values from ZV_QinbarR (PETSc) into ZV_Qin_dam (Fortran)
+
+
+!*******************************************************************************
+!Read file
+!*******************************************************************************
+if (rank==0) then 
+     ZV_Qout_dam=0 
+!     call dam_linear(ZV_Qin_dam,ZV_Qout_dam)
+     ZV_Qout_dam=ZV_Qin_dam
+end if
+
+
+!*******************************************************************************
+!Set values in PETSc vector
+!*******************************************************************************
+if (rank==0) then
+     call VecSetValues(ZV_Qdam,IS_dam_bas,IV_dam_loc2,                         &
+                       ZV_Qout_dam(IV_dam_index),INSERT_VALUES,ierr)
+end if
+
+
+!*******************************************************************************
+!Assemble PETSc vector
+!*******************************************************************************
+call VecAssemblyBegin(ZV_Qdam,ierr)
+call VecAssemblyEnd(ZV_Qdam,ierr)           
+
+
+!*******************************************************************************
+!Optional - Write information in stdout 
+!*******************************************************************************
+!if (rank==0) print *, 'Qin_dam =', ZV_Qin_dam
+!if (rank==0) print *, 'Qout_dam=', ZV_Qout_dam
+!call VecView(ZV_Qdam,PETSC_VIEWER_STDOUT_WORLD,ierr)
+
+
+!*******************************************************************************
+!End 
+!*******************************************************************************
+
+end subroutine rapid_get_Qdam
