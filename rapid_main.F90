3ce8e59fadb0a0f0dfedcd5551c9babe35583c83
Cedric H. David <chdavid@uci.edu>
2014-04-02 17:21:47 -0700

    m3_riv_file, Qout_nc_file --> Vlat_file, Qout_file. Now calling subroutines for open/read/write/close

--- rapid_main.F90
+++ rapid_main.F90
@@ -17,8 +17,8 @@ use rapid_var, only :                                                          &
                    namelist_file,                                              &
                    IS_reachbas,                                                &
                    IV_basin_id,IV_basin_index,IV_basin_loc,                    &
-                   m3_nc_file,Qfor_file,                                       &
-                   Qout_file,V_file,Qout_nc_file,                              &
+                   Vlat_file,Qfor_file,                                        &
+                   Qout_file,V_file,                                           &
                    IS_M,JS_M,JS_RpM,IS_RpM,                                    &
                    ZS_TauR,                                                    &
                    ZV_pnorm,                                                   &
@@ -35,8 +35,8 @@ use rapid_var, only :                                                          &
                    IS_reachtot,IS_forcingbas,                                  &
                    IV_forcing_index,IV_forcing_loc,                            &
                    ZS_time1,ZS_time2,ZS_time3,                                 &
-                   IS_nc_status,IS_nc_id_fil_m3,IS_nc_id_fil_Qout,             &
-                   IS_nc_id_var_m3,IS_nc_id_var_Qout,IS_nc_id_var_comid,       &
+                   IS_nc_status,IS_nc_id_fil_Vlat,IS_nc_id_fil_Qout,           &
+                   IS_nc_id_var_Vlat,IS_nc_id_var_Qout,IS_nc_id_var_comid,     &
                    IS_nc_id_dim_time,IS_nc_id_dim_comid,IV_nc_id_dim,          &
                    IV_nc_start,IV_nc_count,IV_nc_count2,                       &
                    BS_opt_forcing,IS_opt_run
@@ -93,44 +93,14 @@ if (IS_opt_run==1) then
 !-------------------------------------------------------------------------------
 !Create Qout file
 !-------------------------------------------------------------------------------
-if (rank==0) then 
-IS_nc_status=NF90_CREATE(Qout_nc_file,NF90_CLOBBER,IS_nc_id_fil_Qout)
-IS_nc_status=NF90_DEF_DIM(IS_nc_id_fil_Qout,'Time',NF90_UNLIMITED,             &
-                          IS_nc_id_dim_time)
-IS_nc_status=NF90_DEF_DIM(IS_nc_id_fil_Qout,'COMID',IS_reachbas,               &
-                          IS_nc_id_dim_comid)
-IS_nc_status=NF90_DEF_VAR(IS_nc_id_fil_Qout,'COMID',NF90_INT,                  &
-                          IS_nc_id_dim_comid,IS_nc_id_var_comid)
-IV_nc_id_dim(1)=IS_nc_id_dim_comid
-IV_nc_id_dim(2)=IS_nc_id_dim_time
-IS_nc_status=NF90_DEF_VAR(IS_nc_id_fil_Qout,'Qout',NF90_REAL,                  &
-                          IV_nc_id_dim,IS_nc_id_var_Qout)
-IS_nc_status=NF90_ENDDEF(IS_nc_id_fil_Qout)
-IS_nc_status=NF90_PUT_VAR(IS_nc_id_fil_Qout,IS_nc_id_var_comid,IV_basin_id)
-if (rank==0) IS_nc_status=NF90_CLOSE(IS_nc_id_fil_Qout)
-end if
+call rapid_create_Qout_file(Qout_file)
 
 !-------------------------------------------------------------------------------
 !Open files          
 !-------------------------------------------------------------------------------
-if (rank==0) then 
-open(99,file=m3_nc_file,status='old')
-close(99)
-IS_nc_status=NF90_OPEN(m3_nc_file,NF90_NOWRITE,IS_nc_id_fil_m3)
-IS_nc_status=NF90_INQ_VARID(IS_nc_id_fil_m3,'m3_riv',IS_nc_id_var_m3)
-end if
-if (rank==0) then 
-open(99,file=Qout_nc_file,status='old')
-close(99)
-IS_nc_status=NF90_OPEN(Qout_nc_file,NF90_WRITE,IS_nc_id_fil_Qout)
-IS_nc_status=NF90_INQ_VARID(IS_nc_id_fil_Qout,'Qout',IS_nc_id_var_Qout)
-end if
-if (BS_opt_forcing) open(34,file=Qfor_file,status='old')
-
-!open(40,file=Qout_file)
-!open(41,file=V_file)
-!open(99,file='QoutR_900s.dat')
-!if (BS_opt_forcing) open(34,file=Qfor_file,status='old')
+call rapid_open_Qout_file(Qout_file)
+call rapid_open_Vlat_file(Vlat_file)
+if (BS_opt_forcing) call rapid_open_Qfor_file(Qfor_file)
 
 !-------------------------------------------------------------------------------
 !Read, compute and write          
@@ -148,38 +118,17 @@ do JS_M=1,IS_M
 !- + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - +  
 !Read/set upstream forcing
 !- + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - +  
-if (BS_opt_forcing .and. IS_forcingbas > 0) then 
-read(34,*) ZV_read_forcingtot
-call VecSetValues(ZV_Qfor,IS_forcingbas,IV_forcing_loc,                        &
-                  ZV_read_forcingtot(IV_forcing_index),INSERT_VALUES,ierr)
-                  !here we only look at the forcing within the basin studied 
-call VecAssemblyBegin(ZV_Qfor,ierr)
-call VecAssemblyEnd(ZV_Qfor,ierr)           !set Qfor based on reading a file
-!Qfor is only available everyday, this is why it's not included in the following 
-!loop.
-!call PetscPrintf(PETSC_COMM_WORLD,'ZV_Qfor' // char(10),ierr)
-!call VecView(ZV_Qfor,PETSC_VIEWER_STDOUT_WORLD,ierr)
-!call VecSet(ZV_Qfor,0*ZS_one,ierr)
-end if
+if (BS_opt_forcing .and. IS_forcingbas>0) then
+     call rapid_read_Qfor_file
+end if 
 
 do JS_RpM=1,IS_RpM
 
 !- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 !Read/set surface and subsurface volumes 
 !- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-!read(31,*) ZV_read_reachtot
-if (rank==0) then
-IS_nc_status=NF90_GET_VAR(IS_nc_id_fil_m3,IS_nc_id_var_m3,                     &
-                          ZV_read_reachtot,IV_nc_start,IV_nc_count)
-call VecSetValues(ZV_Vlat,IS_reachbas,IV_basin_loc,                            &
-                  ZV_read_reachtot(IV_basin_index),INSERT_VALUES,ierr)
-end if
-call VecAssemblyBegin(ZV_Vlat,ierr)
-call VecAssemblyEnd(ZV_Vlat,ierr)           !set Vlat based on reading a file
+call rapid_read_Vlat_file
 
-!- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-!calculation of Q based on V (here first order explicit approx) 
-!- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 call VecCopy(ZV_Vlat,ZV_Qlat,ierr)            !Qlat=Vlat
 call VecScale(ZV_Qlat,1/ZS_TauR,ierr)         !Qlat=Qlat/TauR
 
@@ -210,36 +159,11 @@ call VecCopy(ZV_VR,ZV_VinitR,ierr)
 !- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 !write outputs         
 !- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-call VecScatterBegin(vecscat,ZV_QoutbarR,ZV_SeqZero,                           &
-                     INSERT_VALUES,SCATTER_FORWARD,ierr)
-call VecScatterEnd(vecscat,ZV_QoutbarR,ZV_SeqZero,                             &
-                        INSERT_VALUES,SCATTER_FORWARD,ierr)
-call VecGetArrayF90(ZV_SeqZero,ZV_pointer,ierr)
-if (rank==0) IS_nc_status=NF90_PUT_VAR(IS_nc_id_fil_Qout,IS_nc_id_var_Qout,    &
-                                       ZV_pointer,IV_nc_start,IV_nc_count2)
-call VecRestoreArrayF90(ZV_SeqZero,ZV_pointer,ierr)
+call rapid_write_Qout_file
 
 if (rank==0) IV_nc_start(2)=IV_nc_start(2)+1
 !do not comment out if writing directly from the routing subroutine
 
-!call VecScatterBegin(vecscat,ZV_QoutbarR,ZV_SeqZero,                           &
-!                     INSERT_VALUES,SCATTER_FORWARD,ierr)
-!call VecScatterEnd(vecscat,ZV_QoutbarR,ZV_SeqZero,                             &
-!                        INSERT_VALUES,SCATTER_FORWARD,ierr)
-!if (rank==0) write (40,'(10e10.3)') ZV_pointer
-!if (rank==0) IS_nc_status=NF90_PUT_VAR(IS_nc_id_fil_Qout,IS_nc_id_var_Qout,    &
-!                                       ZV_pointer,IV_nc_start,IV_nc_count2)
-!call VecRestoreArrayF90(ZV_SeqZero,ZV_pointer,ierr)
-
-!call VecScatterBegin(vecscat,ZV_VbarR,ZV_SeqZero,                              &
-!                     INSERT_VALUES,SCATTER_FORWARD,ierr)
-!call VecScatterEnd(vecscat,ZV_VbarR,ZV_SeqZero,                                &
-!                        INSERT_VALUES,SCATTER_FORWARD,ierr)
-!call VecGetArrayF90(ZV_SeqZero,ZV_pointer,ierr)
-!if (rank==0) write (41,'(10e10.3)') ZV_pointer
-!call VecRestoreArrayF90(ZV_SeqZero,ZV_pointer,ierr)
-
-!call VecView(ZV_QoutbarR,PETSC_VIEWER_STDOUT_WORLD,ierr)
 
 end do
 end do
@@ -264,14 +188,9 @@ call PetscPrintf(PETSC_COMM_WORLD,'Output data created'//char(10),ierr)
 !-------------------------------------------------------------------------------
 !Close files          
 !-------------------------------------------------------------------------------
-if (rank==0) IS_nc_status=NF90_CLOSE(IS_nc_id_fil_m3)
-if (rank==0) IS_nc_status=NF90_CLOSE(IS_nc_id_fil_Qout)
-if (BS_opt_forcing) close(34)
-
-!close(40)
-!close(41)
-!close(99)
-!if (BS_opt_forcing) close(34)
+call rapid_close_Qout_file
+call rapid_close_Vlat_file
+if (BS_opt_forcing) call rapid_close_Qfor_file(Qfor_file)
 
 
 !-------------------------------------------------------------------------------
