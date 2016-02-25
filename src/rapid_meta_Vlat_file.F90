c097d0ccb734cd62009e6d42147c2fc0a37578be
Cedric H. David <cedric.david@jpl.nasa.gov>
2016-02-25 12:19:52 -0800

    Added GET_VAR for time and time_bnds. Safely obtained variable values. Checked time consistency w/ namelist.

--- src/rapid_meta_Vlat_file.F90
+++ src/rapid_meta_Vlat_file.F90
@@ -15,12 +15,12 @@ subroutine rapid_meta_Vlat_file(Vlat_file)
 use netcdf
 use rapid_var, only :                                                          &
                    rank,IS_nc_status,IS_nc_id_fil_Vlat,                        &
-                   IS_nc_id_var_time,IS_nc_id_var_crs,                         &
+                   IS_nc_id_var_time,IS_nc_id_var_time_bnds,IS_nc_id_var_crs,  &
                    IS_nc_id_var_lon,IS_nc_id_var_lat,                          &
-                   IS_riv_tot,IS_riv_bas,                                      &
+                   IS_riv_tot,IS_riv_bas,IS_time,JS_time,ZS_TauR,              &
                    YV_title,YV_institution,YV_comment,                         &
                    YV_time_units,YV_crs_sma,YV_crs_iflat,                      &
-                   ZV_riv_tot_lon,ZV_riv_tot_lat
+                   ZV_riv_tot_lon,ZV_riv_tot_lat,IV_time,IM_time_bnds
 
 implicit none
 
@@ -63,17 +63,56 @@ end if
 
 
 !*******************************************************************************
-!Read coordinates
+!Read space and time variable values
 !*******************************************************************************
 if (rank==0) then 
-     !print *, ZV_riv_tot_lon
+     if (IS_nc_id_var_lon>=0) then
      IS_nc_status=NF90_GET_VAR(IS_nc_id_fil_Vlat,IS_nc_id_var_lon,             \
                                ZV_riv_tot_lon,(/1/),(/IS_riv_tot/))
-     !print *, IS_nc_status
-     !print *, ZV_riv_tot_lon
-     
+     end if
+     if (IS_nc_id_var_lat>=0) then
      IS_nc_status=NF90_GET_VAR(IS_nc_id_fil_Vlat,IS_nc_id_var_lat,             \
                                ZV_riv_tot_lat,(/1/),(/IS_riv_tot/))
+     end if
+     if (IS_nc_id_var_time>=0) then
+     IS_nc_status=NF90_GET_VAR(IS_nc_id_fil_Vlat,IS_nc_id_var_time,            \
+                               IV_time,(/1/),(/IS_time/))
+     end if
+     if (IS_nc_id_var_time_bnds>=0) then
+     IS_nc_status=NF90_GET_VAR(IS_nc_id_fil_Vlat,IS_nc_id_var_time_bnds,       \
+                               IM_time_bnds,(/1,1/),(/2,IS_time/))
+     end if
+end if
+
+
+!*******************************************************************************
+!Check temporal consistency if metadata present
+!*******************************************************************************
+if (IV_time(1)/=-9999) then
+     do JS_time=1,IS_time-1
+     if (IV_time(JS_time+1)-IV_time(JS_time)/=int(ZS_TauR)) then
+     !Checking that interval between values of the time variable is ZS_TauR
+          print '(a53)','Inconsistent time intervals in namelist and Vlat_file'
+          stop 99
+     end if
+     end do
+end if
+
+if (IM_time_bnds(1,1)/=-9999) then
+     do JS_time=1,IS_time-1
+     if (IM_time_bnds(1,JS_time+1)-IM_time_bnds(1,JS_time)/=int(ZS_TauR)) then
+     !Checking that interval between values of the time_bnd variable is ZS_TauR
+          print '(a53)','Inconsistent time intervals in namelist and Vlat_file'
+          stop 99
+     end if
+     end do
+     do JS_time=1,IS_time
+     if (IM_time_bnds(2,JS_time)-IM_time_bnds(1,JS_time)/=int(ZS_TauR)) then
+     !Checking that interval for each value of the time_bnd variable is ZS_TauR
+          print '(a53)','Inconsistent time intervals in namelist and Vlat_file'
+          stop 99
+     end if
+     end do
 end if
 
 
