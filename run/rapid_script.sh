6160bbf659827587fdbfc354a290edea505463a4
Cedric H. David <cedric.david@jpl.nasa.gov>
2016-05-23 13:24:37 -0700

    Added comments and minor reformatting.

--- run/rapid_script.sh
+++ run/rapid_script.sh
@@ -17,13 +17,16 @@
 cd $(dirname ${BASH_SOURCE[0]})
 
 FILE=$(date +"%Y-%m-%d_%H-%M-%S_rapid_stdout.txt")
+
 /usr/bin/time mpiexec                  \
               -n 1                     \
               ./rapid                  \
               -ksp_type richardson     \
               1>$FILE 2>>$FILE
 
-#FILE is a name created based on the time when the model started running
-#FILE contains stdout from running the model (through 1), but also stderr 
-#(through 2).  The output of the time function is also included because 
-#it is located in located in 2.
+#FILE is a name created based on the time when the model started running. FILE 
+#contains stdout from running the model (through 1), but also stderr (through 
+#2). The output of the time function is also included because it is located in 
+#stderr.
+#The statement including BASH_SOURCE[0] allows running this script from other 
+#directories.
