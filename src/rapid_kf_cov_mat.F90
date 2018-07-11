668458fd9494edad06e1cd6c03d3d46843f3fe1c
Charlotte Marie Emery <charlotte.emery@jpl.nasa.gov>
2018-07-10 17:10:46 -0700

    Subroutine rapid_cov_mat.F90 (#12)
    * rename downstream covariances array into ZV_riv_tot_cdownQlat
    
    * update non-zeros counting procedure to include symetric elements
    
    * change preallocation routines to those for matrices of type aij
    
    * update population of matrix with symmetric elements
    
    * add PetscPrintf notice at the end of the routine
    
    * new subroutine for data assimilation: compute matrix that maps daily average runoff into daily average discharge
    
    * new subroutine for data assimilation: compute the kalman filter observation operator ZM_H from the runoff-to-discharge matrix and the selection off observed reaches
    
    * new subroutine for data assimilation: compute the control error covariance matrices PbHT and HPbHT used to estimate the Kalman gain from the runoff error covariance matrix and the observation operator
    
    * new subroutine for data assimilation: compute the analysis/updated control vector using Kalman Filter update equation

---
+++ src/rapid_kf_cov_mat.F90
@@ -0,0 +1,85 @@
+!*******************************************************************************
+!Subroutine - rapid_kf_cov_mat
+!*******************************************************************************
+subroutine rapid_kf_cov_mat
+
+
+!Purpose:
+!Compute runoff error covariance matrices in Kalman gain
+!ZM_HPbt = (ZM_Pb*ZM_H^(T))^(T)
+!ZM_HPbHt = ZM_H*ZM_Pb*ZM_H^(T)
+!Authors: 
+!Charlotte M. Emery, and Cedric H. David, 2018-2018.
+
+!*******************************************************************************
+!Declaration of variables
+!*******************************************************************************
+
+
+
+use rapid_var, only :                                                          &
+                ZM_Pb,ZM_H,ZM_HPbt,ZM_HPbHt,ierr
+                
+implicit none
+
+
+!*******************************************************************************
+!Includes
+!*******************************************************************************
+#include "petsc/finclude/petscsys.h"
+!base PETSc routines
+#include "petsc/finclude/petscvec.h"
+#include "petsc/finclude/petscvec.h90"
+!vectors, and vectors in Fortran90
+#include "petsc/finclude/petscmat.h"
+!matrices
+#include "petsc/finclude/petscksp.h"
+!Krylov subspace methods
+#include "petsc/finclude/petscpc.h"
+!preconditioners
+#include "petsc/finclude/petscviewer.h"
+!viewers (allows writing results in file for example)
+
+!*******************************************************************************
+!Local variables
+!*******************************************************************************
+
+Mat :: ZM_Ht
+
+!*******************************************************************************
+!Compute matrix ZM_HPbt : H*Pb^(T) = H*Pb as Pb symmetric
+!*******************************************************************************
+
+call MatMatMult(ZM_H,                      &
+                ZM_Pb,                     &
+                MAT_INITIAL_MATRIX,        &
+                PETSC_DEFAULT_REAL,        &
+                ZM_HPbt,                   &
+                ierr)
+
+!*******************************************************************************
+!Compute matrix ZM_HPbHt
+!*******************************************************************************
+
+call MatTranspose(ZM_H,MAT_INITIAL_MATRIX,ZM_Ht,ierr)
+
+call MatMatMult(ZM_HPbt,                   &
+                ZM_Ht,                     &
+                MAT_INITIAL_MATRIX,        &
+                PETSC_DEFAULT_REAL,        &
+                ZM_HPbHt,                   &
+                ierr)
+
+call PetscPrintf(PETSC_COMM_WORLD,'Kalman Filter control error covariance matrix created'  &
+                                  //char(10),ierr)
+
+!*******************************************************************************
+!Delete temporary variables
+!*******************************************************************************
+
+call MatDestroy(ZM_Ht,ierr)
+
+!*******************************************************************************
+!End subroutine 
+!*******************************************************************************
+end subroutine rapid_kf_cov_mat
