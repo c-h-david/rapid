96f1cab9030760d97f0b28e6ee7071caf4b95b8f
Cedric H. David <cedric.david@jpl.nasa.gov>
2019-05-30 15:21:32 -0700

    New script to update authorship year.

---
+++ rapid_apprise_year.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#*******************************************************************************
+#rapid_apprise_year.sh
+#*******************************************************************************
+
+#Purpose:
+#This script allows updating the authorship year for all RAPID files. 
+#Author:
+#Cedric H. David, 2019-2019.
+
+
+#*******************************************************************************
+#Check command line arguments
+#*******************************************************************************
+if [ "$#" -ne "2" ]; then
+     echo "ERROR - A total of 2 arguments must be provided" 1>&2
+     exit 22
+fi 
+#Check that 2 arguments were provided
+
+if ! [[ $1 =~ ^[0-9]+$ ]] || [[ ${#1} != 4 ]]; then
+     echo "ERROR - Argument 1 must be a year"
+     exit 22
+fi
+#Check that argument 1 is a year
+
+if ! [[ $2 =~ ^[0-9]+$ ]] || [[ ${#2} != 4 ]]; then
+     echo "ERROR - Argument 2 must be a year"
+     exit 22
+fi
+#Check that argument 2 is a year
+
+
+#*******************************************************************************
+#Assign command line arguments to local variables
+#*******************************************************************************
+old_year=$1
+new_year=$2
+
+echo "- Changing the authorship year in all files from $old_year to $new_year"
+
+
+#*******************************************************************************
+#Change year in ./ directory
+#*******************************************************************************
+for file in `find ./ -maxdepth 1 -type f` 
+do
+     sed -i -e "s|\-$old_year|\-$new_year|" $file
+done
+
+echo " . Done for ./ directory"
+
+
+#*******************************************************************************
+#Change year in ./src/ directory
+#*******************************************************************************
+for file in `find ./src/ -maxdepth 1 -type f` 
+do
+     sed -i -e "s|\-$old_year|\-$new_year|" $file
+done
+
+echo " . Done for ./src/ directory"
+
+
+#*******************************************************************************
+#Change year in ./tst/ directory
+#*******************************************************************************
+for file in `find ./tst/ -maxdepth 1 -type f` 
+do
+     sed -i -e "s|\-$old_year|\-$new_year|" $file
+done
+
+echo " . Done for ./tst/ directory"
+
+
+#*******************************************************************************
+#end
+#*******************************************************************************
