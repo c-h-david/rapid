317d9a30dee28ca9b390169065bc0d7be519b6eb
Cedric H. David <cedric.david@jpl.nasa.gov>
2019-05-30 15:16:32 -0700

    Updated authorship year.

--- rtk/rtk_run_comp.f90
+++ rtk/rtk_run_comp.f90
@@ -15,7 +15,7 @@ program rtk_run_comp
 ! - 22 if netCDF fonctions crash (arguments are faulty) 
 ! - 99 if one of the comparisons failed 
 !Author:
-!Cedric H. David, 2012-2018.
+!Cedric H. David, 2012-2019.
 
 
 !*******************************************************************************
